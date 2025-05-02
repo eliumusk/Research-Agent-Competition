cfcfffb05d8bd779b87e9bb6af667415c8c3315b
javayhu <javayhu@gmail.com>
2025-05-03 00:12:52 +0800

    custom: set locales to en only

--- src/config/website.tsx
+++ src/config/website.tsx
@@ -49,10 +49,10 @@ export const websiteConfig: WebsiteConfig = {
         flag: '🇺🇸',
         name: 'English',
       },
-      zh: {
-        flag: '🇨🇳',
-        name: '中文',
-      },
+      // zh: {
+      //   flag: '🇨🇳',
+      //   name: '中文',
+      // },
     },
   },
   blog: {
