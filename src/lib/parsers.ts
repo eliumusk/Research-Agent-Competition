b230002c3d287cd2e359851f255e0731233dd892
javayhu <javayhu@gmail.com>
2025-11-12 23:32:44 +0800

    refactor: move config/data-table.ts to data-table-config.ts

--- src/lib/parsers.ts
+++ src/lib/parsers.ts
@@ -1,7 +1,7 @@
 import { createParser } from "nuqs/server";
 import { z } from "zod";
 
-import { dataTableConfig } from "@/config/data-table";
+import { dataTableConfig } from "@/components/data-table/data-table-config";
 
 import type {
   ExtendedColumnFilter,
